6a0278cab2f19cedd20fd75ac72650379c31d53d
kxitosxmx <sdila8337@gmail.com>
2024-12-01 20:51:15 +0530

    Major Change +File Rename +Article Categorization +More fxml (Newsarticle/BrowserHistory/UserProfile/ManageUser) +More controllers -Removed Unwanted classes/methods

--- src/main/java/org/example/Nyro/Article.java
+++ src/main/java/org/example/Nyro/Article.java
@@ -26,20 +26,34 @@ public class Article {
            this.imgURL = imgURL;
     }
 
-    public Article(String title, Timestamp date, String description, String source, String url, String imgURL) {
+    public Article(int id, String title, Timestamp date, String description, String content, String source, String url, String imgURL) {
+        this.id = id;
         this.title = title;
         this.publishedAt = date;
         this.description = description;
         this.source = source;
+        this.content = content;
         this.url = url;
         this.imgURL = imgURL;
     }
 
     public Article(int id, String title, String description, String content) {
-        this.id = id;
         this.title = title;
+        this.id = id;
         this.description = description;
         this.content = content;
+    }
+
+    public Article(int id, String title, Timestamp publishedAt, String content, String description, String source, String url, String imgURL, String category) {
+        this.title = title;
+        this.publishedAt = publishedAt;
+        this.id = id;
+        this.content = content;
+        this.category = category;
+        this.description = description;
+        this.source = source;
+        this.url = url;
+        this.imgURL = imgURL;
     }
 
     // Getters
@@ -67,5 +81,20 @@ public class Article {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", publishedAt=" + publishedAt +
+                ", description='" + description + '\'' +
+                ", source='" + source + '\'' +
+                ", url='" + url + '\'' +
+                ", imgURL='" + imgURL + '\'' +
+                ", category='" + category + '\'' +
+                ", keywords=" + keywords +
+                '}';
     }
 }
